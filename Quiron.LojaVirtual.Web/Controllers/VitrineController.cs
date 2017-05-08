e05c3909a867dc4c25cbba66a851131a713760ed
uottoni <uottoni@gmail.com>
2017-05-08 12:47:05 -0300

    paginacao helpers

--- Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
+++ Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Quiron.LojaVirtual.Dominio.Repositorio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,14 @@ namespace Quiron.LojaVirtual.Web.Controllers
 {
     public class VitrineController : Controller
     {
+        private ProdutosRepositorio _repo;
         // GET: Vitrine
         public ActionResult ListarProdutos(int pagina = 1)
         {
-
-            return View();
+            int itensPorPagina = 5;
+            _repo = new ProdutosRepositorio();
+            var ret = (from x in _repo.Produtos select x).Skip((pagina - 1) * itensPorPagina).Take(itensPorPagina);
+            return View(ret);
         }
     }
 }
