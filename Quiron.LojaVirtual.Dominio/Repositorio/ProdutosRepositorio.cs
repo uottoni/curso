67ce8eff82fe6e91682595d4d20af41c4fe78a60
uottoni <uottoni@gmail.com>
2017-05-08 04:21:18 -0300

    4

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutosRepositorio
+    {
+        private readonly QuironContext _context = new QuironContext();
+
+        public IEnumerable<Produto> Produtos
+        {
+            get { return _context.Produtos; }
+        }
+    }
+}
---
+++ Quiron.LojaVirtual.Dominio/Repositorio/QuironContext.cs
@@ -0,0 +1,26 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class QuironContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+
+        }
+
+        //public System.Data.Entity.DbSet<Quiron.LojaVirtual.Dominio.Entidade.Produto> Produtoes { get; set; }
+    }
+}
