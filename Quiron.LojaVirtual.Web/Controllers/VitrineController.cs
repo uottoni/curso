edad03ade6a963137dbc1949b0a9150a98e0d018
uottoni <uottoni@gmail.com>
2017-05-24 12:02:17 -0300

    fix errors

--- Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
+++ Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -17,53 +17,54 @@ namespace Quiron.LojaVirtual.Web.Controllers
             
             int itensPorPagina = 5;
             _repo = new ProdutosRepositorio();
-            ProdutosViewModel model = new ProdutosViewModel();
+            ProdutosViewModel model = new ProdutosViewModel()
+            {
+                Produtos = (from x in _repo.Produtos select x)
+                .Where(x => categoria == null || x.Categoria == categoria)
+                .OrderBy(x => x.Descricao)
+                .Skip((pagina - 1) * itensPorPagina)
+                .Take(itensPorPagina),
+                Paginacao = new Paginacao()
+                {
+                    PaginaAtual = pagina,
+                    ItensPorPagina = itensPorPagina,
+                    ItensTotal = _repo.Produtos.Count(x => categoria == null || x.Categoria == categoria)
+                },
+                UserIP = HttpContext.Request.UserHostAddress,
+                CategoriaAtual = categoria
+
+
+            };
+
+            //if (string.IsNullOrEmpty(categoria))
             //{
-            //    Produtos = (from x in _repo.Produtos select x)
-            //    .Where(x => x.Categoria == null || x.Categoria == categoria)
-            //    .OrderBy(x => x.Descricao)
-            //    .Skip((pagina - 1) * itensPorPagina)
-            //    .Take(itensPorPagina),
-            //    Paginacao = new Paginacao()
+            //    var query = (from x in _repo.Produtos select x)
+            //        .OrderBy(x => x.Descricao);
+
+            //    model.Produtos = query.Skip((pagina - 1) * itensPorPagina).Take(itensPorPagina);
+
+            //    model.Paginacao = new Paginacao()
             //    {
             //        PaginaAtual = pagina,
             //        ItensPorPagina = itensPorPagina,
-            //        ItensTotal = _repo.Produtos.Count(x => x.Categoria == null || x.Categoria == categoria)
-            //    },
-            //    UserIP = HttpContext.Request.UserHostAddress,
-            //    CategoriaAtual = categoria
-              
-            //};
+            //        ItensTotal = query.Count()
+            //    };
 
-            if (string.IsNullOrEmpty(categoria))
-            {
-                var query = (from x in _repo.Produtos select x)
-                    .OrderBy(x => x.Descricao);
+            //}
+            //else
+            //{
+            //    var query = (from x in _repo.Produtos select x).Where(x=>x.Categoria.ToUpper().Contains(categoria.ToUpper()))
+            //     .OrderBy(x => x.Descricao);
 
-                model.Produtos = query.Skip((pagina - 1) * itensPorPagina).Take(itensPorPagina);
+            //    model.Produtos = query.Skip((pagina - 1) * itensPorPagina).Take(itensPorPagina);
 
-                model.Paginacao = new Paginacao()
-                {
-                    PaginaAtual = pagina,
-                    ItensPorPagina = itensPorPagina,
-                    ItensTotal = query.Count()
-                };
-                    
-            }
-            else
-            {
-                var query = (from x in _repo.Produtos select x).Where(x=>x.Categoria.ToUpper().Contains(categoria.ToUpper()))
-                 .OrderBy(x => x.Descricao);
-
-                model.Produtos = query.Skip((pagina - 1) * itensPorPagina).Take(itensPorPagina);
-
-                model.Paginacao = new Paginacao()
-                {
-                    PaginaAtual = pagina,
-                    ItensPorPagina = itensPorPagina,
-                    ItensTotal = query.Count()
-                };
-            }
+            //    model.Paginacao = new Paginacao()
+            //    {
+            //        PaginaAtual = pagina,
+            //        ItensPorPagina = itensPorPagina,
+            //        ItensTotal = query.Count()
+            //    };
+            //}
             //var ret = (from x in _repo.Produtos select x).Skip((pagina - 1) * itensPorPagina).Take(itensPorPagina);
             return View(model);
         }
